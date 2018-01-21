ab0d5bcb43dfb619366bb962dcd85cae55a2c436
Alex Hendren <alex@oa-sys.io>
2018-01-21 14:47:37 -0600

    Initialize canopen corto package

---
+++ include/canopen.h
@@ -0,0 +1,29 @@
+/* canopen.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef CANOPEN_H
+#define CANOPEN_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <canopen/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <canopen/_type.h>
+#include <canopen/_interface.h>
+#include <canopen/_load.h>
+#include <canopen/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
