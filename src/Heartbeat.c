76c7f2b69e555cf2c287482bc13d87250db97464
Alex Hendren <alex@oa-sys.io>
2018-02-01 12:32:29 -0600

    Initialize heartbeat service

---
+++ src/Heartbeat.c
@@ -0,0 +1,27 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/canopen.h>
+int16_t canopen_Heartbeat_construct(
+    canopen_Heartbeat this)
+{
+    /* Insert implementation */
+}
+
+int16_t canopen_Heartbeat_start(
+    canopen_Heartbeat this)
+{
+    /* Insert implementation */
+}
+
+int16_t canopen_Heartbeat_stop(
+    canopen_Heartbeat this)
+{
+    /* Insert implementation */
+}
+
+void canopen_Heartbeat_destruct(
+    canopen_Heartbeat this)
+{
+    /* Insert implementation */
+}
+
---
+++ src/SocketCAN.c
@@ -0,0 +1,21 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/canopen.h>
+
+int16_t canopen_SocketCAN_construct(
+    canopen_SocketCAN this)
+{
+    return 0;
+}
+
+int16_t canopen_SocketCAN_receive(
+    canopen_SocketCAN this)
+{
+    return 0;
+}
+
+int16_t canopen_SocketCAN_transmit(
+    canopen_SocketCAN this)
+{
+    return 0;
+}
