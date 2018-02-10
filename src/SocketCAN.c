c54ee062e3eddc2e47dc31962c86cb7f41a1b347
Alex Hendren <alex@oa-sys.io>
2018-02-10 08:53:38 -0600

    Bind SocketCAN driver interfaace

--- src/SocketCAN.c
+++ src/SocketCAN.c
@@ -1,12 +1,38 @@
 /* This is a managed file. Do not delete this comment. */
 
 #include <include/canopen.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <linux/can/error.h>
+
 int16_t canopen_SocketCAN_construct(
     canopen_SocketCAN this)
 {
-    if (this->id <= 0) {
+    if (this->id < 0) {
         corto_throw("Invalid device id [%d].", this->id);
         goto error;
+    }
+
+    /* Create and bind socket */
+    struct sockaddr_can addr;
+
+    this->fd = socket(AF_CAN, SOCK_RAW, CAN_RAW);
+    if (this->fd < 0) {
+        corto_throw("Device ID [%d] Failed to CREATE [AF_CAN] socket ID [%d]",
+            this->id, this->fd);
+        goto error;
+    } else {
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = this->id;
+        int ret = bind(this->fd, (struct sockaddr*) &addr, sizeof(addr));
+        if (ret) {
+            corto_throw("Device ID [%d] Failed to BIND [AF_CAN] socket ID [%d] | %s",
+                this->id,
+                this->fd,
+                strerror(ret));
+            goto error;
+        }
     }
 
     return 0;
